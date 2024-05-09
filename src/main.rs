90968cbb214277065b8036f04da5bb28b12b7403
elenakrittik <dev.elenakrittik@gmail.com>
2024-05-09 17:00:47 +0300

    refactor: Better display + include bright color versions.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,46 @@
+#![feature(iter_next_chunk)]
+
 use yansi::{Color, Paint};
 
-const PAIRS: [(&str, Color); 9] = [
-    ("Black", Color::Black),
-    ("Primary", Color::Primary),
-    ("Red", Color::Red),
-    ("Green", Color::Green),
-    ("Yellow", Color::Yellow),
-    ("Blue", Color::Blue),
-    ("Magenta", Color::Magenta),
-    ("Cyan", Color::Cyan),
-    ("White", Color::White),
+const COLORS: [Color; 16] = [
+    Color::Black,
+    Color::BrightBlack,
+    Color::Red,
+    Color::BrightRed,
+    Color::Green,
+    Color::BrightGreen,
+    Color::Yellow,
+    Color::BrightYellow,
+    Color::Blue,
+    Color::BrightBlue,
+    Color::Magenta,
+    Color::BrightMagenta,
+    Color::Cyan,
+    Color::BrightCyan,
+    Color::White,
+    Color::BrightWhite,
 ];
 
 fn main() {
-    for (text, color) in PAIRS {
-        eprintln!("This is what {} looks like!", text.fg(color));
+    eprintln!(" Normal | Bright       ");
+    eprintln!("------- | -------------");
+
+    let mut colors = COLORS.into_iter();
+
+    while let Ok([normal, bright]) = colors.next_chunk::<2>() {
+        // HACK: workaround for std::fmt not respecting width in {:?}
+        {
+            let padding = 7 - format!("{:?}", normal).len();
+            //            ^   ----------------------- easiest way to do ".to_debug_string()"
+            //            |
+            //            length of "Normal" underline
+
+            eprint!("{}", " ".repeat(padding));
+        }
+
+        eprint!("{:?}", normal.fg(normal).bold());
+        eprint!(" | ");
+        eprint!("{:?}", bright.fg(bright).bold());
+        eprintln!();
     }
 }
